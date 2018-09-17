cb579f3f618e74a00dbc47084eead874131cd854
Truong <truongae@gmail.com>
2018-09-17 15:40:50 -0400

    add method interface declaration for IIonSequence

--- IonDotnet/IIonSequence.cs
+++ IonDotnet/IIonSequence.cs
@@ -1,9 +1,71 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace IonDotnet
 {
     public interface IIonSequence : IIonContainer, IList<IIonValue>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns> the element at the given index, not null</returns>
+        /// <exception cref="NullValueException"> if the return value is null</exception>
+        /// <exception cref="ArgumentOutOfRangeException"> if the index is out of range</exception>
+        IIonValue Get(int index);
+
+        /// <summary>
+        /// Append a child value to the end of this sequence
+        /// If this sequence is null, then it becomes a singe element sequence
+        /// </summary>
+        /// <param name="child">is the value appended to the sequence</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">if <param name="child"></param> is null</exception>
+        /// <exception cref="ContainedValueException">if <param name="child"></param>is already part of the sequence</exception>
+        /// <exception cref="ArgumentException"> if <param name="child"></param>is a <see cref="IIonDatagram"/></exception>
+        new bool Add(IIonValue child);
+        
+        /// <summary>
+        /// Provides a factory that when invoked constructs a new value and add it to this sequence
+        /// TODO: add more explanation and examples here
+        /// </summary>
+        /// <returns></returns>
         IValueFactory Add();
+
+        /// <summary>
+        /// Insert a child value at the specific position in the sequence
+        /// If the sequence is null, then it becomes a single element sequence
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="child"> the element appended to the sequence</param>
+        /// <exception cref="ArgumentNullException">if child is null</exception>
+        /// <exception cref="ContainedValueException"> if child is already part of the sequence</exception>
+        /// <exception cref="ArgumentException"> if <param name="child"></param>is a <see cref="IIonDatagram"/></exception>
+        /// <exception cref="ArgumentOutOfRangeException"> if the index is out of range</exception>
+        void Add(int index, IIonValue child);
+
+        /// <summary>
+        ///  Provides a factory that when invoked constructs a new value and add it to this sequence
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        IValueFactory Add(int index);
+
+        IIonValue Set(int index, IIonValue element);
+        IIonValue Remove(int index);
+        bool Remove(object o);
+        bool RemoveAll(ICollection c);
+        bool RetainAll(ICollection c);
+        bool Contains(object o);
+        bool ContainsAll(ICollection c);
+        int IndexOf(object o);
+        int LastIndexOf(object o);
+        bool AddAll(ICollection<IIonValue> c);
+        bool AddAll(int index, ICollection<IIonValue> c);
+        IEnumerator<IIonValue> ListIterator(int index);
+        List<IIonValue> SubList(int from, int to);
+        IIonValue[] ToArray();
+        
     }
 }
--- IonDotnet/Internals/Lite/IonSequenceLite.cs
+++ IonDotnet/Internals/Lite/IonSequenceLite.cs
@@ -1,4 +1,7 @@
-﻿namespace IonDotnet.Internals.Lite
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IonDotnet.Internals.Lite
 {
     internal abstract class IonSequenceLite : IonContainerLite, IIonSequence
     {
@@ -10,10 +13,120 @@
         {
         }
 
-        public abstract int IndexOf(IIonValue item);
-        public abstract void Insert(int index, IIonValue item);
-        public abstract void RemoveAt(int index);
-        public abstract IIonValue this[int index] { get; set; }
-        public abstract IValueFactory Add();
+        public int IndexOf(IIonValue item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(int index, IIonValue item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IIonValue this[int index]
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        public IIonValue Get(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Add(IIonValue child)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IValueFactory Add()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(int index, IIonValue child)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IValueFactory Add(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IIonValue Set(int index, IIonValue element)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IIonValue Remove(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(object o)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RemoveAll(ICollection c)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RetainAll(ICollection c)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(object o)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ContainsAll(ICollection c)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int IndexOf(object o)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int LastIndexOf(object o)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AddAll(ICollection<IIonValue> c)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AddAll(int index, ICollection<IIonValue> c)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<IIonValue> ListIterator(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<IIonValue> SubList(int @from, int to)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IIonValue[] ToArray()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
